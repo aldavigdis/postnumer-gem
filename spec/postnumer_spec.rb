6152eb214330e546b1d3eedd20e690e85bc73c01
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2023-02-05 14:30:15 +0100

    Using eq instead of be in specs

--- spec/postnumer_spec.rb
+++ spec/postnumer_spec.rb
@@ -6,24 +6,24 @@ RSpec.describe Postnumer do
   end
 
   it 'finds locality from postal code using a class method' do
-    expect(Postnumer.locality(310)).to be 'Borgarnes'
-    expect(Postnumer.locality('101')).to be 'Reykjavík'
+    expect(Postnumer.locality(310)).to eq 'Borgarnes'
+    expect(Postnumer.locality('101')).to eq 'Reykjavík'
   end
 
   it 'finds locality from postal code using an instance method' do
     postnumer = Postnumer.new(310)
-    expect(postnumer.locality).to be 'Borgarnes'
+    expect(postnumer.locality).to eq 'Borgarnes'
 
     postnumer_from_str = Postnumer.new('101')
-    expect(postnumer_from_str.locality).to be 'Reykjavík'
+    expect(postnumer_from_str.locality).to eq 'Reykjavík'
   end
 
   it 'finds validity of a postal code using a class method' do
-    expect(Postnumer.valid?(310)).to be true
-    expect(Postnumer.valid?('101')).to be true
-    expect(Postnumer.valid?(999)).to be false
-    expect(Postnumer.valid?('foo')).to be false
-    expect(Postnumer.valid?(1)).to be false
-    expect(Postnumer.valid?(-310)).to be false
+    expect(Postnumer.valid?(310)).to eq true
+    expect(Postnumer.valid?('101')).to eq true
+    expect(Postnumer.valid?(999)).to eq false
+    expect(Postnumer.valid?('foo')).to eq false
+    expect(Postnumer.valid?(1)).to eq false
+    expect(Postnumer.valid?(-310)).to eq false
   end
 end
